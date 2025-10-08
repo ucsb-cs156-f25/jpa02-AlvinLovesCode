d215081344bed058772e5aec6b4501d4e3663797
Alvin Chan <alvinchan102020005@gmail.com>
2025-10-08 10:40:21 -0700

    xy - updated tests for Team.java

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -19,7 +19,34 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+    
+    @Test
+    public void equals_works_as_expected() {
+        Team team1 = new Team("test-team");
+        assert(team.equals(team1));
+
+        assert(team.equals(team));
+        
+        Team team2 = new Team("another-team");
+        assert(!team.equals(team2));
+
+        assert(!team.equals(null));
+
+    }
+
+    @Test
+    public void hashCode_works_as_expected() {
+        Team t = new Team();
+        int result = t.hashCode();
+        int expectedResult = 1;
+        assertEquals(expectedResult, result);
+
+    }
+
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
